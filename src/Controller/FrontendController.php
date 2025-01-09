6cd0a0ce91ca5d08f80759bfdd95f47ca2b952f4
Leats92 <staelelangmane@outlook.fr>
2025-01-09 14:49:30 +0100

    commit 3 stael

---
+++ src/Controller/FrontendController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PokemonRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+class FrontendController extends AbstractController
+{
+    private const ITEMS_PER_PAGE = 12;
+
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request, PokemonRepository $pokemonRepository): Response
+    {
+        $page = max(1, $request->query->getInt('page', 1));
+
+        $queryBuilder = $pokemonRepository->createQueryBuilder('p')
+            ->orderBy('p.name', 'ASC');
+
+        $queryBuilder
+            ->setFirstResult(($page - 1) * self::ITEMS_PER_PAGE)
+            ->setMaxResults(self::ITEMS_PER_PAGE);
+
+        $paginator = new Paginator($queryBuilder);
+        $totalItems = count($paginator);
+        $totalPages = ceil($totalItems / self::ITEMS_PER_PAGE);
+
+        return $this->render('pokemon/index.html.twig', [
+            'pokemons' => $paginator,
+            'currentPage' => $page,
+            'totalPages' => $totalPages,
+            'totalItems' => $totalItems
+        ]);
+    }
+}
